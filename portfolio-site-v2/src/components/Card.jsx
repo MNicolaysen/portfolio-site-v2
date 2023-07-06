cf29a90c924953f38df145cdfe14b2258226020b
Morton Nicolaysen <mort1995@hotmail.com>
2023-07-06 16:20:55 +0100

    mobile styling

--- portfolio-site-v2/src/components/Card.jsx
+++ portfolio-site-v2/src/components/Card.jsx
@@ -69,38 +69,42 @@ function Card({
             <h3 className={Styles["card-title"]}>{title}</h3>
             <div className={Styles["card-text"]}>
               <p>{description}</p>
-              <h4>
-                {framework === "React.js" || framework === "Ruby on Rails"
-                  ? "Framework:"
-                  : "Language:"}{" "}
-                {framework}
-              </h4>
-              <h4>Date created: {dateCreated}</h4>
-              <div className={Styles["button-group"]}>
-                {linkOne && (
-                  <button
-                    className={Styles["card-button"]}
-                    onClick={handleButton1Click}
-                  >
-                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                  </button>
-                )}
-                {linkTwo && (
-                  <button
-                    className={Styles["card-button"]}
-                    onClick={handleButton2Click}
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </button>
-                )}
-                {linkThree && (
-                  <button
-                    className={Styles["card-button"]}
-                    onClick={handleButton3Click}
-                  >
-                    <i className="fa-brands fa-youtube"></i>
-                  </button>
-                )}
+              <div className={Styles["card-details-bottom"]}>
+                <div className={Styles["sub-details"]}>
+                  <h4>
+                    {framework === "React.js" || framework === "Ruby on Rails"
+                      ? "Framework:"
+                      : "Language:"}{" "}
+                    {framework}
+                  </h4>
+                <h4>Date created: {dateCreated}</h4>
+                </div>
+                <div className={Styles["button-group"]}>
+                  {linkOne && (
+                    <button
+                      className={Styles["card-button"]}
+                      onClick={handleButton1Click}
+                    >
+                      <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                    </button>
+                  )}
+                  {linkTwo && (
+                    <button
+                      className={Styles["card-button"]}
+                      onClick={handleButton2Click}
+                    >
+                      <i className="fa-brands fa-github"></i>
+                    </button>
+                  )}
+                  {linkThree && (
+                    <button
+                      className={Styles["card-button"]}
+                      onClick={handleButton3Click}
+                    >
+                      <i className="fa-brands fa-youtube"></i>
+                    </button>
+                  )}
+              </div>
               </div>
             </div>
             <div className={Styles["video-container"]}>
